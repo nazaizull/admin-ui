b54c5d75964bd09f3edd376288d56b60fe6ca78c
nazaizull <nazaizul>
2024-10-28 21:13:17 +0700

    tutorialiconsclear

--- src/components/Fragments/Header.jsx
+++ src/components/Fragments/Header.jsx
@@ -1,5 +1,5 @@
 import Input from "../Elements/LabeledInput/Input";
-//import NotificationsIcon from "@mui/icons-material/Notifications";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 const Header = () =>{
     const date = new Date().toDateString().slice(4);
@@ -7,13 +7,13 @@ const Header = () =>{
     return(
     <header className="border-b-2 p-6 flex items-center justify-between">
         <div className="flex">
-            <div className="font-bold text-lg">Naza</div>
+            <div className="font-bold text-lg">Username</div>
             <div className="ms-6 text-gray-03">{date}</div>
         </div>
         <div className="flex">
-        <div className="self-center">
-          Logo
-         </div>
+            <div>
+                <NotificationsIcon/>
+            </div>
             <div className="ms-10 hidden sm:block">
                 <Input variant="bg-white w-80"/>
             </div>
